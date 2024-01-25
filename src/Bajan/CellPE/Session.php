89eaf32acdcfaaa5c608194b9a8f6c7389f35c1f
BajanVlogs <kjolly544@gmail.com>
2024-01-24 22:36:47 -0500

    Update Session.php

--- src/Bajan/CellPE/Session.php
+++ src/Bajan/CellPE/Session.php
@@ -2,83 +2,71 @@
 
 namespace Bajan\CellPE;
 
-
-class Session{
+class Session {
 
     /** @var CellPE */
     private $plugin;
-    private $name;
-    private $price;
+    private $name = [];
+    private $price = [];
     /** @var array */
     private $session = [];
 
-    public function __construct(CellPE $plugin){
+    public function __construct(CellPE $plugin) {
         $this->plugin = $plugin;
     }
 
     /**
      * @param $p
-     * @return string
+     * @return string|null
      */
-
-    public function searchName($p){
-        return $this->name[$p];
+    public function searchName($p) {
+        return $this->name[$p] ?? null;
     }
 
     /**
      * @param $p
      * @param $name
      */
-
-    public function setName($p, $name){
+    public function setName($p, $name) {
         $this->name[$p] = $name;
     }
 
     /**
      * @param $p
-     * @return mixed
+     * @return mixed|null
      */
-
-    public function searchPrice($p){
-        return $this->price[$p];
+    public function searchPrice($p) {
+        return $this->price[$p] ?? null;
     }
 
     /**
      * @param $p
      * @param $price
      */
-
-    public function setPrice($p, $price){
+    public function setPrice($p, $price) {
         $this->price[$p] = $price;
     }
 
     /**
      * @param $name
-     * @return mixed
+     * @return mixed|null
      */
-
-    public function getSession($name){
-        if(isset($this->session[$name])){
-            return $this->session[$name];
-        }
-        return null;
+    public function getSession($name) {
+        return $this->session[$name] ?? null;
     }
 
     /**
      * @param $name
      * @param $session
      */
-
-    public function setSession($name, $session){
+    public function setSession($name, $session) {
         $this->session[$name] = $session;
     }
 
     /**
      * @param $name
      */
-
-    public function removeSession($name){
+    public function removeSession($name) {
         unset($this->session[$name]);
     }
-
 }
